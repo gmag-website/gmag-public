a69e756be69e5b8fc898e713303d7b9f65ecbe27
gosan-publish <actions@users.noreply.github.com>
2026-09-01 02:01:26 +0200

    publish 11 essay(s)

--- page-misc.jsx
+++ page-misc.jsx
@@ -120,7 +120,7 @@ function Credentials() {
       <h2 className="gsn-display cred-title">شناسنامهٔ شماره</h2>
       <dl className="cred-list">
         <div className="cred-row">
-          <dt>مدیر مسئول و سردبیر</dt>
+          <dt>مدیرمسئول و سردبیر</dt>
           <dd>یلدا زمانی</dd>
         </div>
         <div className="cred-row">
@@ -181,7 +181,7 @@ function AboutManifesto() {
                 <dd>اندیشکدهٔ فرهنگ و هنر گوسان<br /><span style={{ direction: 'ltr', display: 'inline-block' }}>Gōsān Institute e. V. i. Gr.</span></dd>
               </div>
               <div className="cred-row">
-                <dt>مدیر مسئول و سردبیر</dt>
+                <dt>مدیرمسئول و سردبیر</dt>
                 <dd>یلدا زمانی</dd>
               </div>
               <div className="cred-row">
